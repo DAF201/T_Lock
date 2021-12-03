059066d7934ecff6fbf4f506ee65ee9f747e389f
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-12-03 12:50:58 -0500

    Create zip_time_test.py

---
+++ T_Lock_test_space/zip_time_test.py
@@ -0,0 +1,21 @@
+import zipfile
+import os
+print(os.path.getctime('t.py'))
+print(os.path.getatime('t.py'))
+print(os.path.getmtime('t.py'))
+with zipfile.ZipFile('t.zip', 'w')as zip:
+    zip.write('t.py')
+os.remove('t.py')
+with zipfile.ZipFile('t.zip', 'r')as zip:
+    zip.extractall()
+print(os.path.getctime('t.py'))
+print(os.path.getatime('t.py'))
+print(os.path.getmtime('t.py'))
+
+# 1638553612.7103932
+# 1638553612.7103932
+# 1638553612.7103932
+
+# 1638553612.7103932
+# 1638553790.0718744
+# 1638553790.0718744
